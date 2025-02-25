e87a8b5aa2a9072b08c8004da92bba205663f67d
Perparimpepe <perparimpepe@gmail.com>
2025-02-25 21:55:03 +0200

    melkein valmis

--- Projekti/toiminnot.js
+++ Projekti/toiminnot.js
@@ -1,6 +1,3 @@
-function painallus() {
-    alert("Tämä on JavaScript-toiminto!");
-}
 function handleSubmit(event) {
     event.preventDefault(); // Estää lomakkeen oletusarvoisen lähettämisen
 
@@ -9,23 +6,30 @@ function handleSubmit(event) {
     var hours = document.getElementById('hours').value;
     var extra = document.getElementById('extra').value;
 
-    // Luo uuden rivin taulukkoon
-    var table = document.getElementById('data').getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow();
+    // Tarkistaa, että category ja hours kentät eivät ole tyhjiä
+    if (category === '' || hours === '') {
+        alert("Täytä kaikki kentät!");
+        return;
+    }
 
-    // Luo uudet solut ja lisää arvot
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-
-    cell1.innerHTML = category;
-    cell2.innerHTML = hours;
-    cell3.innerHTML = extra;
-    
-    // Tallenna tiedot localStorageen
+    // Hakee tallennetut tiedot
     var data = JSON.parse(localStorage.getItem('tableData')) || [];
-    data.push({ category, hours, extra });
+    var existingItem = data.find(item => item.category === category);
+
+    if (existingItem) {
+        // Päivittää olemassa olevan rivin
+        existingItem.hours = parseInt(existingItem.hours) + parseInt(hours);
+        existingItem.extra = existingItem.extra + ' ' + extra;
+    } else {
+        // Luo uuden rivin taulukkoon
+        data.push({ category, hours, extra });
+    }
+
+    // Tallenna tiedot localStorageen
     localStorage.setItem('tableData', JSON.stringify(data));
+
+    // Päivittää taulukon
+    loadTableData();
 
     // Tyhjentää lomakkeen kentät
     document.getElementById('category').value = '';
@@ -37,6 +41,9 @@ function loadTableData() {
     var data = JSON.parse(localStorage.getItem('tableData')) || [];
     var table = document.getElementById('data').getElementsByTagName('tbody')[0];
     
+    // Tyhjentää taulukon ennen lataamista
+    table.innerHTML = '';
+
     data.forEach(function(item) {
         var newRow = table.insertRow();
         var cell1 = newRow.insertCell(0);
@@ -55,3 +62,29 @@ window.onload = loadTableData;
 // Lisää tapahtumankuuntelija lomakkeelle
 document.querySelector('form').addEventListener('submit', handleSubmit);
 
+function summary() {
+    var data = JSON.parse(localStorage.getItem('tableData')) || [];
+    var totalHours = 0;
+    var extra = '';
+
+    data.forEach(function(item) {
+        totalHours += parseInt(item.hours) || 0;
+        extra += item.extra + ' ';
+    });
+
+    var summaryText = 'Summary: ' + totalHours + ' hours and ' + extra.trim() + ' extra information';
+    document.getElementById('summaryText').value = summaryText;
+}
+    function clearData() {
+    // Poistaa tiedot localStoragesta
+    localStorage.removeItem('tableData');
+
+    // Päivittää taulukon
+    loadTableData();
+
+    // Tyhjentää yhteenvedon tekstin
+    document.getElementById('summaryText').value = '';
+}
+
+// Lisää tapahtumankuuntelija tyhjennyspainikkeelle
+document.getElementById('clearButton').addEventListener('click', clearData);
